f5c69ed0fe68e9ce8fc418ba75577c1cc4ece0e5
Leonid <Lenya.m1998@gmail.com>
2020-07-30 08:47:33 +0300

    set timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //'use strict';
 document.addEventListener('DOMContentLoaded', () => {
+   //tabs
+
    const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector('.tabheader__items');
@@ -34,4 +36,56 @@ document.addEventListener('DOMContentLoaded', () => {
       }
    });
 
+   //timer
+
+   const deadLine = '2020-08-07';
+
+
+
+   function timeUntilEnd(endtime) {
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+         days = Math.round(t / 1000 / 60 / 60 / 24),
+         hours = Math.round((t / 1000 / 60 / 60) % 24),
+         minutes = Math.round((t / 1000 / 60) % 60),
+         seconds = Math.round((t / 1000) % 60);
+      return {
+         remainingTime: t,
+         days: days,
+         hours: hours,
+         minutes: minutes,
+         seconds: seconds
+      };
+   }
+
+   function plusZero(num) {
+      if (num >= 0 && num < 10) {
+         return `0${num}`;
+      } else {
+         return num;
+      }
+   }
+
+   function setTimer(endtime) {
+      const timer = document.querySelector('.timer'),
+         days = timer.querySelector('#days'),
+         hours = timer.querySelector('#hours'),
+         minutes = timer.querySelector('#minutes'),
+         seconds = timer.querySelector('#seconds'),
+         timerUpdate = setInterval(updateClock, 1000);
+
+      updateClock();
+
+      function updateClock() {
+         const t = timeUntilEnd(endtime);
+         days.innerHTML = plusZero(t.days);
+         hours.innerHTML = plusZero(t.hours);
+         minutes.innerHTML = plusZero(t.minutes);
+         seconds.innerHTML = plusZero(t.seconds);
+
+         if (t.remainingTime <= 0) {
+            clearInterval(timerUpdate);
+         }
+      }
+   }
+   setTimer(deadLine);
 });
